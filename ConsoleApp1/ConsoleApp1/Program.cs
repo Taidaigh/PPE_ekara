0ba006115e15ecabc227cc6baf06f6806174e351
Pierre <pierre.dealmeida@yahoo.fr>
2018-12-04 17:24:41 +0100

    NIA NIA NIA

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Salut");
+            Console.WriteLine("NIA NIA NIA NIA NIA NIA");
         }
     }
 }
